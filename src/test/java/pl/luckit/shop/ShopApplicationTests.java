36d35890ee965278bd869daa4da2fd0af2ae7b88
lucki90 <tomlucki@wp.pl>
2022-11-12 13:42:31 +0100

    WS-1 initial commit

---
+++ src/test/java/pl/luckit/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.luckit.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
